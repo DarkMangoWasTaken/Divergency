6a062a615b37830cd73ce8927d6d1393450137c5
Brise <kingstevefett@gmail.com>
2022-06-06 12:59:26 +0200

    idk

---
+++ Items/Accs/Forest/HealLeafProjectile.cs
@@ -0,0 +1,82 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.DataStructures;
+using System;
+
+namespace DivergencyMod.Items.Accs.Forest
+{
+    public class HealLeafProjectile : ModProjectile
+    {
+        public int timer = 1;
+
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = 4;
+        }
+        public override void SetDefaults()  //currently a damaging projectile
+        {
+            Projectile.width = 10;
+            Projectile.height = 10;
+
+            Projectile.friendly = true;
+            Projectile.penetrate = 1;
+            Projectile.tileCollide = true;
+            Projectile.DamageType = DamageClass.Generic;
+            Projectile.ownerHitCheck = false;
+            Projectile.extraUpdates = 0;
+            Projectile.timeLeft = 6000;
+        }
+
+        public override void OnSpawn(IEntitySource source)
+        {
+            Projectile.velocity = Vector2.Zero;
+            Projectile.velocity.Y = 2f;          //change this to increase the fall speed of the projectile
+        }
+
+        public override void AI()
+        {
+            base.AI();
+
+            if (++Projectile.frameCounter >= 6)
+            {
+                Projectile.frameCounter = 0;
+                if (++Projectile.frame >= 4)
+                {
+                    Projectile.frame = 0;
+                }
+            }
+
+
+
+            if (timer < 100)
+            {
+                if (timer == 0)
+                {
+                    Projectile.velocity = Projectile.velocity.RotatedBy(2);
+                    timer++;
+                }
+
+                Projectile.velocity = Projectile.velocity.RotatedBy(0.02);
+                timer++;
+            }
+            else if (timer < 200)
+            {
+                if (timer == 100)
+                {
+                    Projectile.velocity = Projectile.velocity.RotatedBy(-2);
+                    timer++;
+                }
+
+                Projectile.velocity = Projectile.velocity.RotatedBy(-0.02);
+                timer++;
+            }
+            else if (timer == 200)
+            {
+                timer = 0;
+            }
+        }
+    }
+}
--- Items/Accs/Forest/LivingWoodConductor.cs
+++ Items/Accs/Forest/LivingWoodConductor.cs
@@ -48,7 +48,7 @@ namespace DivergencyMod.Items.Accs.Forest
             // Reset our equipped flag. If the accessory is equipped somewhere, ExampleShield.UpdateAccessory will be called and set the flag before PreUpdateMovement
             HeartHeal = false;
         }
-
+            
         public override void OnHitNPC(Item item, NPC target, int damage, float knockback, bool crit)
         {
             if (Player.HeldItem.DamageType == DamageClass.Magic && HeartHeal)
@@ -58,7 +58,7 @@ namespace DivergencyMod.Items.Accs.Forest
 
                     for (int i = 0; i < Main.rand.Next(3, 4); i++)
                     {
-                        Projectile.NewProjectile(target.GetSource_FromThis(), target.Center, new Vector2(Main.rand.Next(-3, 3), Main.rand.Next(-15, -10)), ModContent.ProjectileType<LivingStone>(), target.damage, 1f, Player.whoAmI);
+                        Projectile.NewProjectile(target.GetSource_FromThis(), target.Center, new Vector2(Main.rand.Next(-3, 3) * 0.2f, Main.rand.Next(-15, -10) * 0.2f), ModContent.ProjectileType<HealLeafProjectile>(), target.damage, 1f, Player.whoAmI);
                     }
 
                 }
@@ -74,7 +74,7 @@ namespace DivergencyMod.Items.Accs.Forest
 
                 for (int i = 0; i < Main.rand.Next(3, 4); i++)
                 {
-                    Projectile.NewProjectile(target.GetSource_FromThis(), target.Center, new Vector2(Main.rand.Next(-3, 3) * 1.4f, Main.rand.Next(-15, -10)) * 1.4f, ModContent.ProjectileType<LivingStone>(), target.damage, 1f, Player.whoAmI);
+                    Projectile.NewProjectile(target.GetSource_FromThis(), target.Center, new Vector2(Main.rand.Next(-3, 3) * 0.2f, Main.rand.Next(-15, -10)) * 0.2f, ModContent.ProjectileType<HealLeafProjectile>(), target.damage, 1f, Player.whoAmI);
                 }
 
             }
