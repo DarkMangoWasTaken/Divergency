65405bd3af7dbe7dd81c9909992f83a47df03925
catNull <benvb2004@gmail.com>
2022-09-19 01:13:45 +0200

    Making Living core bow

---
+++ Items/Weapons/Ranged/LivingCoreBow/LivingCoreBow.cs
@@ -0,0 +1,51 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DivergencyMod.Items.Weapons.Ranged.LivingCoreBow
+{
+    public class LivingCoreBow : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Living Core Bow"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+            Tooltip.SetDefault(" -- | O_O | -- ");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 12;
+            Item.DamageType = DamageClass.Ranged;
+            Item.width = 0;
+            Item.height = 0;
+            Item.useTime = 50;
+            Item.useAnimation = 50;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.knockBack = 2;
+            Item.value = 10000;
+            Item.noMelee = true;
+            Item.rare = ItemRarityID.Green;
+            Item.crit = 10;
+            Item.autoReuse = true;
+            Item.shoot = ModContent.ProjectileType<Projectiles.Weapons.Ranged.LivingCoreArrow>();
+            Item.shootSpeed = 20f;
+            Item.useAmmo = AmmoID.Arrow;
+            // Item.scale = 1.4f;
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<Projectiles.Weapons.Ranged.LivingCoreArrow>(), damage, knockback, player.whoAmI);
+
+            return false; // base.Shoot(player, source, position, velocity, ModContent.ProjectileType<Projectiles.Weapons.Ranged.LivingCoreArrow>(), damage, knockback);
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-8, 0);
+        }
+    }
+}
---
+++ Projectiles/Weapons/Ranged/LivingCoreArrow.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+namespace DivergencyMod.Projectiles.Weapons.Ranged
+{
+    internal class LivingCoreArrow : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.aiStyle = Terraria.ID.ProjAIStyleID.Arrow;
+            Projectile.damage = 10;
+            Projectile.width = 14;
+            Projectile.height = 32;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.friendly = true;
+            Projectile.arrow = true;
+            Projectile.tileCollide = true;
+
+            Projectile.ai[0] = 0;
+        }
+
+        public override void AI()
+        {
+            if (Projectile.ai[0] < 0)
+            {
+                Projectile.ai[0] -= 1;
+                if (Projectile.ai[0] == -91)
+                {
+                    Projectile.Kill();
+                    Terraria.Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, new Vector2(0, -6f), ModContent.ProjectileType<LivingCoreTip>(), Projectile.damage, Projectile.knockBack, Projectile.owner);
+                }
+            }
+        }
+
+        public override bool PreAI()
+        {
+            if (!(Projectile.ai[0] >= 0))
+                AI();
+
+            return Projectile.ai[0] >= 0;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Projectile.ai[0] = -1;
+
+            Projectile.position += oldVelocity;
+            Projectile.velocity = new Vector2(0, 0);
+            Projectile.tileCollide = false;
+
+            return false;
+        }
+    }
+}
