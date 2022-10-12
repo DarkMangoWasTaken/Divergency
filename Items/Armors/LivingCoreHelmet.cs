ef876dbf481d0d6dc4c98c7ab87b2d4058160911
Kackbrise <kingstevefett@gmail.com>
2022-10-12 11:50:48 +0200

    smol changeridoos

--- Items/Armors/LivingCoreHelmet.cs
+++ Items/Armors/LivingCoreHelmet.cs
@@ -47,7 +47,7 @@ namespace DivergencyMod.Items.Armors
 
 			player.GetDamage(DamageClass.Melee) += 0.10f; // Increase dealt damage for all weapon classes by 5%
 			player.GetCritChance(DamageClass.Melee) += 8;
-			
+
 			//player.GetAttackSpeed(DamageClass.Melee) += 0.7f;
 
 		}
@@ -107,8 +107,8 @@ namespace DivergencyMod.Items.Armors
 					Vector2 speed = Main.rand.NextVector2Unit() * 0.02f;
 					ParticleManager.NewParticle(Player.Top + new Vector2(0, 10), speed * 30, ParticleManager.NewInstance<FancyParticle>(), Color.Purple, 0.4f, Player.whoAmI);
 
-                }
-                if (ShieldReactivateCooldown > 0)
+				}
+				if (ShieldReactivateCooldown > 0)
 				{
 
 					ShieldReactivateCooldown--;
@@ -188,9 +188,9 @@ namespace DivergencyMod.Items.Armors
 				player.GetModPlayer<DivergencyPlayer>().ScreenShakeIntensity = 30;
 
 				player.GetModPlayer<LivingCoreArmorMelee>().RageActive = true;
-                Projectile.NewProjectile(null, player.Center + new Vector2(-20, -30), new Vector2(0), ModContent.ProjectileType<LivingCoreEye>(), 0, 0, player.whoAmI);
+				Projectile.NewProjectile(null, player.Center + new Vector2(-20, -30), new Vector2(0), ModContent.ProjectileType<LivingCoreEye>(), 0, 0, player.whoAmI);
 				player.AddBuff(ModContent.BuffType<LivingCoreRageBuff>(), 300);
-                player.GetModPlayer<LivingCoreArmorMelee>().ShieldReactivate = true;
+				player.GetModPlayer<LivingCoreArmorMelee>().ShieldReactivate = true;
 				player.GetModPlayer<LivingCoreArmorMelee>().ShieldReactivateCooldown = 300;
 				player.GetModPlayer<LivingCoreArmorMelee>().ShieldDamage = 0;
 
@@ -203,10 +203,10 @@ namespace DivergencyMod.Items.Armors
 		int timer;
 		public override void SetStaticDefaults()
 		{
-			DisplayName.SetDefault("Living Core Shield"); 
+			DisplayName.SetDefault("Living Core Shield");
 			ProjectileID.Sets.TrailCacheLength[Projectile.type] = 25; // in SetStaticDefaults()
-            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
-        }
+			ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+		}
 		public override void SetDefaults()
 		{
 			Projectile.alpha = 100;
@@ -222,78 +222,78 @@ namespace DivergencyMod.Items.Armors
 			Projectile.Center = player.Top + new Vector2(0, 10);
 			if (timer == 1)
 			{
-                ParticleManager.NewParticle(Projectile.Center, Projectile.velocity, ParticleManager.NewInstance<LivingCoreEyeParticle>(), Color.Purple, 1f, Projectile.whoAmI, Projectile.whoAmI);
+				ParticleManager.NewParticle(Projectile.Center, Projectile.velocity, ParticleManager.NewInstance<LivingCoreEyeParticle>(), Color.Purple, 1f, Projectile.whoAmI, Projectile.whoAmI);
 
-            }
-        }
+			}
+		}
 
-        public TrailRenderer prim;
-        public TrailRenderer prim2;
-        public override bool PreDraw(ref Color lightColor)
-        {
-            var TrailTex = ModContent.Request<Texture2D>("DivergencyMod/Trails/Trail").Value;
-            Color color = Color.Multiply(new(0.50f, 2.05f, 0.5f, 0), 80);
-            if (prim == null)
-            {
-                prim = new TrailRenderer(TrailTex, TrailRenderer.DefaultPass, (p) => new Vector2(30f) * (1f - p), (p) => Projectile.GetAlpha(Color.LimeGreen) * 0.9f * (float)Math.Pow(1f - p, 2f));
-                prim.drawOffset = Projectile.Size / 2f;
-            }
-            if (prim2 == null)
-            {
-                prim2 = new TrailRenderer(TrailTex, TrailRenderer.DefaultPass, (p) => new Vector2(15f) * (1f - p), (p) => Projectile.GetAlpha(Color.White) * 0.9f * (float)Math.Pow(1f - p, 2f));
-                prim2.drawOffset = Projectile.Size / 2f;
-            }
-            prim.Draw(Projectile.oldPos);
-            prim2.Draw(Projectile.oldPos);
+		public TrailRenderer prim;
+		public TrailRenderer prim2;
+		public override bool PreDraw(ref Color lightColor)
+		{
+			var TrailTex = ModContent.Request<Texture2D>("DivergencyMod/Trails/Trail").Value;
+			Color color = Color.Multiply(new(0.50f, 2.05f, 0.5f, 0), 80);
+			if (prim == null)
+			{
+				prim = new TrailRenderer(TrailTex, TrailRenderer.DefaultPass, (p) => new Vector2(30f) * (1f - p), (p) => Projectile.GetAlpha(Color.LimeGreen) * 0.9f * (float)Math.Pow(1f - p, 2f));
+				prim.drawOffset = Projectile.Size / 2f;
+			}
+			if (prim2 == null)
+			{
+				prim2 = new TrailRenderer(TrailTex, TrailRenderer.DefaultPass, (p) => new Vector2(15f) * (1f - p), (p) => Projectile.GetAlpha(Color.White) * 0.9f * (float)Math.Pow(1f - p, 2f));
+				prim2.drawOffset = Projectile.Size / 2f;
+			}
+			prim.Draw(Projectile.oldPos);
+			prim2.Draw(Projectile.oldPos);
 
 
-            return false;
-        }
-    }
+			return false;
+		}
+	}
 
-    [AutoloadEquip(EquipType.Head)]
-    public class LivingCoreHelmetMage : ModItem
-    {
-        public override void SetStaticDefaults()
-        {
-            base.SetStaticDefaults();
-            DisplayName.SetDefault("Living Core Mage Cowl");
+	[AutoloadEquip(EquipType.Head)]
+	public class LivingCoreHelmetMage : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Living Core Mage Cowl");
 			Tooltip.SetDefault("'It's a perfect fit!'");
-           
-        }
 
-        public override void SetDefaults()
-        {
-            Item.width = 18;
-            Item.height = 18;
-            Item.value = 10000;
-            Item.rare = ItemRarityID.Green;
-            Item.defense = 0;
-        }
+		}
 
-        public override void UpdateEquip(Player player)
-        {
+		public override void SetDefaults()
+		{
+			Item.width = 18;
+			Item.height = 18;
+			Item.value = 10000;
+			Item.rare = ItemRarityID.Green;
+			Item.defense = 0;
+		}
 
-        }
-        public override bool IsArmorSet(Item head, Item body, Item legs)
-        {
-            return body.type == ModContent.ItemType<LivingCoreChestplate>() && legs.type == ModContent.ItemType<LivingCoreGreaves>();
+		public override void UpdateEquip(Player player)
+		{
 
-        }
-        public override void UpdateArmorSet(Player player)
-        {
+		}
+		public override bool IsArmorSet(Item head, Item body, Item legs)
+		{
+			return body.type == ModContent.ItemType<LivingCoreChestplate>() && legs.type == ModContent.ItemType<LivingCoreGreaves>();
 
-            player.setBonus = "Gain a defensive shield, if the shield breaks it lowers your defense but increases offensive stats greatly"// This is the setbonus tooltip
-                            + "\nShield increases defense up to 20, if the shields breaks, the player loses defense'"
-                            + "\nin exchange of bonus melee speed, movement speed and damage)";
+		}
+		public override void UpdateArmorSet(Player player)
+		{
+
+			player.setBonus = "Gain a defensive shield, if the shield breaks it lowers your defense but increases offensive stats greatly"// This is the setbonus tooltip
+							+ "\nShield increases defense up to 20, if the shields breaks, the player loses defense'"
+							+ "\nin exchange of bonus melee speed, movement speed and damage)";
 
 
 
-            player.GetModPlayer<LivingCoreArmorMage>().initialize = true;
+			player.GetModPlayer<LivingCoreArmorMage>().initialize = true;
 
-        }
+		}
 
-    }
+	}
 	public class LivingCoreArmorMage : ModPlayer
 	{
 		public bool initialize;
@@ -316,7 +316,6 @@ namespace DivergencyMod.Items.Armors
 		{
 			if (initialize)
 			{
-				Main.NewText(DoubleTapCounter);
 				if (ActivateTimer)
 				{
 					DoubleTapTimer--;
@@ -328,56 +327,187 @@ namespace DivergencyMod.Items.Armors
 					ActivateTimer = false;
 				}
 
-                if (Player.controlDown)
+				if (Player.controlDown)
 				{
-                    for (int i = 0; i < 1; i++)
-                    {
-                        Vector2 speed = Main.rand.NextVector2Unit() * 0.1f;
-                        ParticleManager.NewParticle(Player.Center + new Vector2(Main.rand.NextFloat(-20, 20)), speed * Main.rand.NextFloat(25, 30), ParticleManager.NewInstance<FancyParticle>(), Color.Purple, 1f, Player.whoAmI, Layer: Layer.BeforeProjectiles);
-                    }
-                    DoubleTapCounter++;
+					for (int i = 0; i < 1; i++)
+					{
+						Vector2 speed = Main.rand.NextVector2Unit() * 0.1f;
+						ParticleManager.NewParticle(Player.Center + new Vector2(Main.rand.NextFloat(-20, 20)), speed * Main.rand.NextFloat(25, 30), ParticleManager.NewInstance<FancyParticle>(), Color.Purple, 1f, Player.whoAmI, Layer: Layer.BeforeProjectiles);
+					}
+					DoubleTapCounter++;
 					ActivateTimer = true;
 				}
 
 				if (DoubleTapCounter >= 120 && DoubleTapTimer <= 0 && !Player.HasBuff(ModContent.BuffType<HealerStance>()) && !Player.HasBuff(ModContent.BuffType<DamageStance>()))
 				{
 					Player.AddBuff(ModContent.BuffType<HealerStance>(), 1);
-                    DoubleTapTimer = 0;
-                    for (int i = 0; i < 30; i++)
-                    {
-                        Vector2 speed = Main.rand.NextVector2Unit() * 0.1f;
-                        ParticleManager.NewParticle(Player.Center + new Vector2(Main.rand.NextFloat(-10, 10)), speed * Main.rand.NextFloat(15, 30), ParticleManager.NewInstance<FancyParticle>(), Color.Purple, 2f, Player.whoAmI, Layer: Layer.BeforeProjectiles);
-                    }
-                }
+					DoubleTapTimer = 0;
+					for (int i = 0; i < 30; i++)
+					{
+						Vector2 speed = Main.rand.NextVector2Unit() * 0.1f;
+						ParticleManager.NewParticle(Player.Center + new Vector2(Main.rand.NextFloat(-10, 10)), speed * Main.rand.NextFloat(15, 30), ParticleManager.NewInstance<FancyParticle>(), Color.Purple, 2f, Player.whoAmI, Layer: Layer.BeforeProjectiles);
+					}
+				}
 				else if (DoubleTapCounter >= 120 && DoubleTapTimer <= 0 && Player.HasBuff(ModContent.BuffType<HealerStance>()))
 				{
-                    Player.ClearBuff(ModContent.BuffType<HealerStance>());
-                    Player.AddBuff(ModContent.BuffType<DamageStance>(), 1);
-                    DoubleTapTimer = 0;
-                    for (int i = 0; i < 3; i++)
-                    {
-                        Vector2 speed = Main.rand.NextVector2Unit() * 0.1f;
-                        ParticleManager.NewParticle(Player.Center + new Vector2(Main.rand.NextFloat(-10, 10)), speed * Main.rand.NextFloat(15, 30), ParticleManager.NewInstance<FancyParticle>(), Color.Purple, 2f, Player.whoAmI, Layer: Layer.BeforeProjectiles);
-                    }
-                }
-                else if (DoubleTapCounter >= 120 && DoubleTapTimer <= 0 && Player.HasBuff(ModContent.BuffType<DamageStance>()))
-                {
-                    Player.ClearBuff(ModContent.BuffType<DamageStance>());
-                    DoubleTapTimer = 0;
-                    for (int i = 0; i < 3; i++)
-                    {
-                        Vector2 speed = Main.rand.NextVector2Unit() * 0.1f;
-                        ParticleManager.NewParticle(Player.Center + new Vector2(Main.rand.NextFloat(-10, 10)), speed * Main.rand.NextFloat(15, 30), ParticleManager.NewInstance<FancyParticle>(), Color.Purple, 2f, Player.whoAmI, Layer: Layer.BeforeProjectiles);
-                    }
-                }
+					Player.ClearBuff(ModContent.BuffType<HealerStance>());
+					Player.AddBuff(ModContent.BuffType<DamageStance>(), 1);
+					DoubleTapTimer = 0;
+					for (int i = 0; i < 3; i++)
+					{
+						Vector2 speed = Main.rand.NextVector2Unit() * 0.1f;
+						ParticleManager.NewParticle(Player.Center + new Vector2(Main.rand.NextFloat(-10, 10)), speed * Main.rand.NextFloat(15, 30), ParticleManager.NewInstance<FancyParticle>(), Color.Purple, 2f, Player.whoAmI, Layer: Layer.BeforeProjectiles);
+					}
+				}
+				else if (DoubleTapCounter >= 120 && DoubleTapTimer <= 0 && Player.HasBuff(ModContent.BuffType<DamageStance>()))
+				{
+					Player.ClearBuff(ModContent.BuffType<DamageStance>());
+					DoubleTapTimer = 0;
+					for (int i = 0; i < 3; i++)
+					{
+						Vector2 speed = Main.rand.NextVector2Unit() * 0.1f;
+						ParticleManager.NewParticle(Player.Center + new Vector2(Main.rand.NextFloat(-10, 10)), speed * Main.rand.NextFloat(15, 30), ParticleManager.NewInstance<FancyParticle>(), Color.Purple, 2f, Player.whoAmI, Layer: Layer.BeforeProjectiles);
+					}
+				}
 
 
-            }
+			}
 		}
-       
-    
+
+
 
 	}
+
+	[AutoloadEquip(EquipType.Head)]
+	public class LivingCoreHelmetSummon : ModItem
+	{
+		public int timer;
+
+		public override void SetStaticDefaults()
+		{
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Living Core Summoner Headgear");
+			Tooltip.SetDefault("'It's a perfect fit!'"
+				+ "\nIncreases your max minions by 2'");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 18;
+			Item.height = 18;
+			Item.value = 10000;
+			Item.rare = ItemRarityID.Green;
+			Item.defense = 0;
+		}
+
+		public override void UpdateEquip(Player player)
+		{
+			player.maxMinions += 2;
+
+		}
+
+		public override bool IsArmorSet(Item head, Item body, Item legs)
+		{
+			return body.type == ModContent.ItemType<LivingCoreChestplate>() && legs.type == ModContent.ItemType<LivingCoreGreaves>();
+
+		}
+		public override void UpdateArmorSet(Player player)
+		{
+
+			player.setBonus = "Increases all summoner stats in the near of Sentries";
+			player.maxTurrets += 1;
+
+			for (int i = 0; i < Main.maxProjectiles; i++)
+			{
+				Projectile proj = Main.projectile[i];
+
+				if (proj.active && player.Distance(proj.Center) <= 300 && proj.sentry)
+				{
+					timer++;
+					player.whipRangeMultiplier += 0.2f;
+					player.GetAttackSpeed(DamageClass.SummonMeleeSpeed) *= 1.2f;
+					player.GetDamage(DamageClass.Summon) *= 1.15f;
+					if (timer == 5)
+					{
+						for (int i2 = 0; i2 < 10; i2++)
+						{
+							Vector2 speed = Main.rand.NextVector2Unit() * 0.1f;
+							ParticleManager.NewParticle(proj.Center + new Vector2(Main.rand.NextFloat(-10, 10)), speed * Main.rand.NextFloat(15, 30), ParticleManager.NewInstance<FancyParticle>(), Color.Purple, 0.5f, proj.whoAmI, Layer: Layer.BeforeProjectiles);
+						}
+						timer = 0;
+					}
+
+
+
+				}
+			}
+		}
+	}
+    [AutoloadEquip(EquipType.Head)]
+    public class LivingCoreHelmetRanged : ModItem
+    {
+        public int timer;
+
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            DisplayName.SetDefault("Living Core Summoner Headgear");
+            Tooltip.SetDefault("'It's a perfect fit!'"
+                + "\nIncreases your max minions by 2'");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 18;
+            Item.height = 18;
+            Item.value = 10000;
+            Item.rare = ItemRarityID.Green;
+            Item.defense = 0;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.maxMinions += 2;
+
+        }
+
+        public override bool IsArmorSet(Item head, Item body, Item legs)
+        {
+            return body.type == ModContent.ItemType<LivingCoreChestplate>() && legs.type == ModContent.ItemType<LivingCoreGreaves>();
+
+        }
+        public override void UpdateArmorSet(Player player)
+        {
+
+            player.setBonus = "Increases all summoner stats in the near of Sentries";
+            player.maxTurrets += 1;
+
+            for (int i = 0; i < Main.maxProjectiles; i++)
+            {
+                Projectile proj = Main.projectile[i];
+
+                if (proj.active && player.Distance(proj.Center) <= 300 && proj.sentry)
+                {
+                    timer++;
+                    player.whipRangeMultiplier += 0.2f;
+                    player.GetAttackSpeed(DamageClass.SummonMeleeSpeed) *= 1.2f;
+                    player.GetDamage(DamageClass.Summon) *= 1.15f;
+                    if (timer == 5)
+                    {
+                        for (int i2 = 0; i2 < 10; i2++)
+                        {
+                            Vector2 speed = Main.rand.NextVector2Unit() * 0.1f;
+                            ParticleManager.NewParticle(proj.Center + new Vector2(Main.rand.NextFloat(-10, 10)), speed * Main.rand.NextFloat(15, 30), ParticleManager.NewInstance<FancyParticle>(), Color.Purple, 0.5f, proj.whoAmI, Layer: Layer.BeforeProjectiles);
+                        }
+                        timer = 0;
+                    }
+
+
+
+                }
+            }
+        }
+    }
 }
 		
  
