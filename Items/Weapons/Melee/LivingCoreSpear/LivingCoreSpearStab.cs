f96892fdf83f65ffbbb10f5d29981a436222ff04
Kackbrise <kingstevefett@gmail.com>
2022-10-24 20:41:01 +0200

    spear stuffs

---
+++ Items/Weapons/Melee/LivingCoreSpear/LivingCoreSpearStab.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DivergencyMod.Items.Weapons.Melee.LivingCoreSpear
+{
+    public class LivingCoreSpearStab : ModProjectile
+    {
+        public override string Texture => "DivergencyMod/Items/Weapons/Melee/LivingCoreSpear/LivingCoreSpear";
+
+        
+      
+
+    }
+}
